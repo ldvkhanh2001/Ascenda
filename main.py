a611949184e8066d7857b23ce0b943596668263e
Khanh Le <ldvkhanh2001@gmail.com>
2023-02-18 12:56:01 +0700

    change main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from api.api import ValidNearByOffers
 
 if __name__ == "__main__":
     apiConnect = "https://61c3deadf1af4a0017d990e7.mockapi.io/offers/near_by?lat=1.313492&lon=103.860359&rad=20" 
-    checkinData = "2020-02-01"
+    checkinData = "2019-12-25"
     responseObject = APIResponse()
     responseData = responseObject.getData(apiConnect=apiConnect)
     configData = ReadConfig()
